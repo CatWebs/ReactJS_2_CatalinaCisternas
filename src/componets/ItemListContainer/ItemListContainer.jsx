843a099441dcd429a3443dfed0cd5b6a93d54134
CatWebs <135657662+CatWebs@users.noreply.github.com>
2024-08-02 10:28:13 -0400

    Update ItemListContainer.jsx

--- src/componets/ItemListContainer/ItemListContainer.jsx
+++ src/componets/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,7 @@
-//import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ItemList from "./ItemList"
 import "./ItemListContainer.css"
-//import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 
 
